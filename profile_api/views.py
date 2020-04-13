ffd048ac3140aad3a5ab329db1297754920f7be5
RAHULBARIK <rahulbarik583@gmail.com>
2020-04-13 13:45:48 +0530

    Test the first apiview using get request

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiview(APIView):
+    """Test API View"""
+
+    def get(self, response, format=None):
+        """Returns a list of APIView features"""
+        an_apiview = [
+            "Uses HTTP methods as function (get,post,put,patch,delete)",
+            "Is a similar to a traditional Django VIew",
+            "Gives you most control over you application logic",
+            "Is mapped manually URLs",
+        ]
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
