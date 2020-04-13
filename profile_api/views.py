4030a96737a72d9003fbb47a4b585b2d0ba8667e
RAHULBARIK <rahulbarik583@gmail.com>
2020-04-13 21:13:43 +0530

    all http response

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,5 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profile_api import serializers
 
 
 class HelloApiview(APIView):
@@ -14,3 +16,31 @@ class HelloApiview(APIView):
             "Is mapped manually URLs",
         ]
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    serializer_class = serializers.HelloSerializer
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle partial upodate of an object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method': 'DELETE'})
